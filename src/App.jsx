c462ec671c6e724e83de15bc5334c283ab97c670
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-12-22 21:14:13 +0530

    created & imported all the elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import Header from './assets/components/header/Header'
+import Nav from './assets/components/nav/Nav'
+import About from './assets/components/about/About'
+import Experience from './assets/components/experience/Experience'
+import Services from './assets/components/services/Services'
+import Portfolio from './assets/components/portfolio/Portfolio'
+import Testimonials from './assets/components/testimonials/Testimonials'
+import Contact from './assets/components/contact/Contact'
+import Footer from './assets/components/footer/Footer'
 
 const app = () => {
   return (
-   <>
-   ghk
-   </>
+  <>
+
+  <Header />
+  <Nav />
+  <About />
+  <Experience />
+  <Services />
+  <Portfolio />
+  <Testimonials />
+  <Contact />
+  <Footer />
+
+  </>
   )
 }
 
